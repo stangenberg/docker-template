b9d65c3f77e9d0bd75c65b7604a41166ecd304c2
Thorben Stangenberg <thorben@Thorbens-iMac.local>
2015-03-20 14:20:30 +0100

    introducing new.sh script

---
+++ new.sh
@@ -0,0 +1,69 @@
+#!/bin/sh
+
+echo "______           _               _____                    _       _       ";
+echo "|  _  \         | |             |_   _|                  | |     | |      ";
+echo "| | | |___   ___| | _____ _ __    | | ___ _ __ ___  _ __ | | __ _| |_ ___ ";
+echo "| | | / _ \ / __| |/ / _ \ '__|   | |/ _ \ '_ \` _ \| '_ \| |/ _\` | __/ _ \ ";
+echo "| |/ / (_) | (__|   <  __/ |      | |  __/ | | | | | |_) | | (_| | ||  __/";
+echo "|___/ \___/ \___|_|\_\___|_|      \_/\___|_| |_| |_| .__/|_|\__,_|\__\___|";
+echo "                                                   | |                    ";
+echo "                                                   |_|                    ";
+echo "                                                                          ";
+echo "                                                                          ";
+echo "                     Creating a new Docker image"
+echo ""
+
+if [ -z $1 ]
+then
+  printf "Please name the new image: "
+  read -r inputname
+fi
+
+if [ "$1" ] 
+then
+  inputname=$1
+fi
+
+
+if [ -z $2 ]
+then
+  printf "What is it about?: "
+  read -r description
+fi
+
+baseurl=${3:-"https://bitbucket.org/thstangenberg/docker-template/raw/master/template"}
+
+imagename=${inputname// /_}
+
+dirname=$(pwd -L)/docker-$imagename
+echo ""
+echo ""
+echo "creating project in $dirname"
+echo ""
+echo ""
+if [ -d $dirname ] 
+then
+	echo "directory $dirname exists already - aborting"
+	exit 1
+fi
+
+mkdir $dirname
+
+load() {
+	echo "Creating $1"
+	curl -# $baseurl/$1 -o $dirname/$1
+}
+
+load Dockerfile
+load LICENSE.md
+load LICENSE.txt
+load Makefile
+load README.md
+
+sed -i .backup "s/imagename/$imagename/g" $dirname/Makefile
+sed -i .backup "s/imagename/$imagename/g" $dirname/README.md
+sed -i .backup "s/description/$description/g" $dirname/README.md
+
+rm -f $dirname/*.backup
+echo ""
+echo "Ready."
